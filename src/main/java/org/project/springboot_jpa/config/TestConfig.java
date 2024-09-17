12737db4eb03b9ecd1764124257470b0e3710dcd
bzanf <borysfreitas@gmail.com>
2024-09-17 19:25:48 -0300

    Create OrderStatus enum and update Order

--- src/main/java/org/project/springboot_jpa/config/TestConfig.java
+++ src/main/java/org/project/springboot_jpa/config/TestConfig.java
@@ -2,6 +2,7 @@ package org.project.springboot_jpa.config;
 
 import org.project.springboot_jpa.entities.Order;
 import org.project.springboot_jpa.entities.User;
+import org.project.springboot_jpa.entities.enums.OrderStatus;
 import org.project.springboot_jpa.repositories.OrderRepository;
 import org.project.springboot_jpa.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Jorge", "jorge@email.com", "988888888", "1234");
         User user2 = new User(null, "Carlos", "carlos@email.com", "977777777", "1234");
 
-        Order order1 = new Order(null, Instant.parse("2023-03-05T10:02:55Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2024-09-03T07:55:21Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2021-11-25T19:21:33Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2023-03-05T10:02:55Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2024-09-03T07:55:21Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2021-11-25T19:21:33Z"), OrderStatus.SHIPPED, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
