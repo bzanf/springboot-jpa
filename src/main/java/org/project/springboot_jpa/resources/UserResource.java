805eb590fb156c8c853c6720f4b5ea65d39f292d
bzanf <borysfreitas@gmail.com>
2024-09-17 17:28:44 -0300

    Create UserService and update UserResource

--- src/main/java/org/project/springboot_jpa/resources/UserResource.java
+++ src/main/java/org/project/springboot_jpa/resources/UserResource.java
@@ -1,17 +1,32 @@
 package org.project.springboot_jpa.resources;
 
 import org.project.springboot_jpa.entities.User;
+import org.project.springboot_jpa.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findOne() {
-        return ResponseEntity.ok().body(new User(1L, "jorge", "jorge@mail.com","4899999999","1234"));
+    public ResponseEntity<List<User>> findOne() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
