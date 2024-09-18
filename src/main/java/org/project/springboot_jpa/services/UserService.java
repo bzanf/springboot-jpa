afd5e408b838a5a313cdde9e65cd6dd5d72282ec
bzanf <borysfreitas@gmail.com>
2024-09-17 21:51:41 -0300

    Add custom exception handling for findById method

--- src/main/java/org/project/springboot_jpa/services/UserService.java
+++ src/main/java/org/project/springboot_jpa/services/UserService.java
@@ -2,6 +2,7 @@ package org.project.springboot_jpa.services;
 
 import org.project.springboot_jpa.entities.User;
 import org.project.springboot_jpa.repositories.UserRepository;
+import org.project.springboot_jpa.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
